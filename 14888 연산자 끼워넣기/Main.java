cfdcaa328023f4d9e1ff04fd7c75b75e70abadc6
tomo <shindohyun00@gmail.com>
2020-04-05 13:33:56 +0900

    14888 연산자 끼워넣기 재풀이

--- 14888 연산자 끼워넣기/Main.java
+++ 14888 연산자 끼워넣기/Main.java
@@ -1,89 +1,70 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int nums[];
-	static int ops[] = new int[4]; //0: +, 1: -, 2: *, 3: /
-	static long min = 1000000000;
-	static long max = -1000000000;
-	static HashMap<String, Long> check = new HashMap<String, Long>();
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		nums = new int[N];
+	static int N; //2~11
+	static int[] digits = new int[11];
+	static int[] opCnts = new int[4];
+	static long min = Long.MAX_VALUE;
+	static long max = Long.MIN_VALUE;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			nums[i] = sc.nextInt();
+			digits[i] = Integer.parseInt(st.nextToken());
 		}
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < 4; i++) {
-			ops[i] = sc.nextInt();
+			opCnts[i] = Integer.parseInt(st.nextToken());
 		}
-		sc.close();
+		br.close();
 		
 		solution();
 		System.out.println(max);
 		System.out.println(min);
 	}
-
+	
 	private static void solution() {
-		dp(ops[0], ops[1], ops[2], ops[3], "");
+		recursive(0, 1, digits[0]);
 	}
 	
-	private static void dp(int add, int sub, int mul, int div, String opStr) {
-		if(add == 0 && sub == 0 && mul == 0 && div == 0) {
-			//check 및 계산
-			if(!check.containsKey(opStr)) {
-				long result = calculate(opStr);
-				if(max < result) {
-					max = result;
-				}
-				
-				if(min > result) {
-					min = result;
-				}
-				check.put(opStr, result);
-			}
+	private static void recursive(int opCnt, int numIdx, int res) {
+		if(opCnt == N-1) {
+			if(res > max) max = res;
+			if(res < min) min = res;
 			return;
 		}
 		
-		if(add > 0) {
-			dp(add - 1, sub, mul, div, opStr + "+");	
-		}
-		if(sub> 0) {
-			dp(add, sub - 1, mul, div, opStr + "-");	
-		}
-		if(mul > 0) {
-			dp(add, sub, mul - 1, div, opStr + "*");	
-		}
-		if(div > 0) {
-			dp(add, sub, mul, div - 1, opStr + "/");	
-		}
+		if(opCnt > N-1 || numIdx >= N) return;
+		
+		for(int i = 0; i < 4; i++) {
+			if(opCnts[i] <= 0) continue;
+			
+			opCnts[i]--;
+			recursive(opCnt+1, numIdx+1, calculate(numIdx, res, i));
+			opCnts[i]++;
+		}		
 	}
 	
-	private static long calculate(String opStr) {
-		long result = 0;
-		int num_index = 0;
+	private static int calculate(int numIdx, int res, int op) {
+		int cur = digits[numIdx];
 		
-		int tempNums[] = nums.clone();
-		
-		for(char op : opStr.toCharArray()) {
-			if(op == '+') {
-				tempNums[num_index + 1] = tempNums[num_index] + tempNums[num_index + 1];
-			}
-			else if(op == '-') {
-				tempNums[num_index + 1] = tempNums[num_index] - tempNums[num_index + 1];
-			}
-			else if(op == '*') {
-				tempNums[num_index + 1] = tempNums[num_index] * tempNums[num_index + 1];
-			}
-			else if(op == '/') {
-				tempNums[num_index + 1] = tempNums[num_index] / tempNums[num_index + 1];
-			}
-			num_index++;
+		switch(op) {
+		case 0:
+			return res + cur;
+		case 1:
+			return res - cur;
+		case 2:
+			return res * cur;
+		case 3:
+			return res / cur;
 		}
 		
-		result = tempNums[num_index];
-		return result;
+		return 0;
 	}
 }
