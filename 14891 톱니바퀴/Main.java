87d9404da71a60dceae103e8b45008ca90a9dfb4
tomo <shindohyun00@gmail.com>
2020-05-04 02:04:06 +0900

    14891 톱니바퀴 재풀이

--- 14891 톱니바퀴/Main.java
+++ 14891 톱니바퀴/Main.java
@@ -1,153 +1,115 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int gs[][] = new int[4][8];
-	static int K;
-	static int gNums[];
-	static int dirs[];
-	static int score = 0;
+	static String[] gs = new String[4];
+	static int K; //1~100
+	static int sum = 0;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		for(int i = 0; i < 4; i++) {	
-			char gCharArr[] = sc.nextLine().toCharArray();
-			
-			for(int j = 0; j < gCharArr.length; j++) {
-				gs[i][j] = gCharArr[j] - '0';
-			}
-		}
-		
-		K = sc.nextInt();
-		gNums = new int[K];
-		dirs = new int[K];
-		
-		for(int i = 0; i < K; i++) {
-			gNums[i] = sc.nextInt() - 1; //인덱스니까
-			dirs[i] = sc.nextInt();
-		}
-		
-		sc.close();
-		
-		solution();
-		System.out.println(score);
-	}
-	
-	private static void solution() {
-		for(int i = 0; i < K; i++) {
-			boolean tryGs[] = new boolean[4]; //회전할 기어들 표시
-			int tryDirs[] = new int[4]; //회전할 기어들의 회전 방향
-			
-			tryGs[gNums[i]] = true;
-			tryDirs[gNums[i]] = dirs[i];
-			
-			//기준의 오른쪽 검사
-			//기준의 2와 오른쪽 기어의 6 비교
-			int curG = gNums[i];
-			int curDir = dirs[i];
-			
-			int sideG = curG + 1;
-			while(sideG < 4) {
-				if(gs[curG][2] != gs[sideG][6]) {
-					tryGs[sideG] = true;
-					tryDirs[sideG] = curDir * -1;
-					
-					curG = sideG;
-					curDir = tryDirs[sideG];
-					sideG++;
-				}
-				else {
-					//멈춘 기어의 오른쪽은 더이상 볼 필요 없다.
-					break;
-				}
-			}
-			
-			//기준의 왼쪽 검사
-			//기준의 6과 왼쪽 기어의 2 비교
-			curG = gNums[i];
-			curDir = dirs[i];
-			
-			sideG = curG - 1;
-			while(sideG >= 0) {
-				if(gs[curG][6] != gs[sideG][2]) {
-					tryGs[sideG] = true;
-					tryDirs[sideG] = curDir * -1;
-					
-					curG = sideG;
-					curDir = tryDirs[sideG];
-					sideG--;
-				}
-				else {
-					//멈춘 기어의 왼쪽은 더이상 볼 필요 없다.
-					break;
-				}
-			}
-			
-			//알맞게 회전시키기
-			for(int j = 0; j < 4; j++) {
-				if(tryGs[j]) {
-					spin(j, tryDirs[j]);
-				}
-			}
-			//printG();
-		}
-		
-		calculateScore();
-	}
-	
-	private static void printG() {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 8; j++) {
-				System.out.print(gs[i][j]);
-			}
-			System.out.println();
+			gs[i] = br.readLine();	
 		}
-		System.out.println();
+		K = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		for(int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			rotation(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+		}
+		br.close();
+	
+		calculate();
+		System.out.println(sum);
 	}
 	
-	private static void spin(int tryG, int dir) {
-		int tryGArr[] = gs[tryG];
-		int cloneArr[] = tryGArr.clone();
-		
-		//시계방향
-		if(dir == 1) {
-			for(int i = 0; i < 8; i++) {
-				if(i == 7) {
-					tryGArr[0] = cloneArr[i];
-				}
-				else {
-					tryGArr[i + 1] = cloneArr[i];	
-				}
+	private static void calculate() {
+		for(int i = 0; i < 4; i++) {
+			if(gs[i].charAt(0) == '1') {
+				sum += Math.pow(2, i);
 			}
 		}
-		//반시계방향
+	}
+	
+	private static void rotation(int idx, int d) {
+		int[] idxs = new int[4];
+		int[] ds = new int[4];		
+		idxs[0] = idx;
+		ds[0] = d;
+		int rotCnt = 1;
+		
+		int curIdx = idx;
+		int curD = d;
+		int leftIdx;
+		//왼쪽 톱니바퀴 탐색
+		while(true) {
+			leftIdx = curIdx-1;
+			if(leftIdx < 0) break;
+			if(gs[leftIdx].charAt(2) != gs[curIdx].charAt(6)) {
+				idxs[rotCnt] = leftIdx;
+				ds[rotCnt] = curD * -1;
+				rotCnt++;
+				curIdx = leftIdx;
+				curD = curD*-1;
+			}
+			else {
+				break;
+			}
+		}
+		
+		curIdx = idx;
+		curD = d;
+		int rightIdx;
+		//오른쪽 톱니바퀴 탐색
+		while(true) {
+			rightIdx = curIdx+1;
+			if(rightIdx >= 4) break;
+			if(gs[rightIdx].charAt(6) != gs[curIdx].charAt(2)) {
+				idxs[rotCnt] = rightIdx;
+				ds[rotCnt] = curD * -1;
+				rotCnt++;
+				curIdx = rightIdx;
+				curD = curD*-1;
+			}
+			else {
+				break;
+			}
+		}
+		
+		for(int i = 0; i < rotCnt; i++) {
+			rotationEx(idxs[i], ds[i]);	
+		}
+	}
+	
+	private static void rotationEx(int idx, int d) {
+		char[] g = gs[idx].toCharArray();
+		
+		if(d==1) {
+			char temp = g[7];
+			g[7] = g[6];
+			g[6] = g[5];
+			g[5] = g[4];
+			g[4] = g[3];
+			g[3] = g[2];
+			g[2] = g[1];
+			g[1] = g[0];
+			g[0] = temp;
+		}
 		else {
-			for(int i = 7; i >= 0; i--) {
-				if(i == 0) {
-					tryGArr[7] = cloneArr[i];
-				}
-				else {
-					tryGArr[i - 1] = cloneArr[i];	
-				}
-			}
+			char temp = g[0];
+			g[0] = g[1];
+			g[1] = g[2];
+			g[2] = g[3];
+			g[3] = g[4];
+			g[4] = g[5];
+			g[5] = g[6];
+			g[6] = g[7];
+			g[7] = temp;
 		}
-	}
-	
-	private static void calculateScore() {
-		if(gs[0][0] == 1) {
-			score += 1;
-		}
-		
-		if(gs[1][0] == 1) {
-			score += 2;
-		}
-		
-		if(gs[2][0] == 1) {
-			score += 4;
-		}
-		
-		if(gs[3][0] == 1) {
-			score += 8;
-		}
+				
+		gs[idx] = new String(g);
 	}
 }
