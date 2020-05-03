55de612f94e0983f14de52b0bbfd72c10dcc66c0
tomo <shindohyun00@gmail.com>
2020-05-04 00:36:22 +0900

    14890 경사로 재풀이

--- 14890 경사로/Main.java
+++ 14890 경사로/Main.java
@@ -1,122 +1,191 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, L;
-	static int map[][];
-	static int cnt = 0;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		L = sc.nextInt();
-		map = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				map[i][j] = sc.nextInt();
+	static int N; //2~100
+	static int L; //1~100
+	static int[][] map = new int[100][100]; //각 1~10
+	static boolean[][] visit = new boolean[100][100]; //사다리 체크
+	static int cnt =0;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		for(int i =0;i<N;i++) {
+			st =new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		sc.close();
+		br.close();
 		
 		solution();
 		System.out.println(cnt);
 	}
 	
-	private static void solution() { 
-		//row check
-		for(int i = 0; i < N; i++) {
-			check(map[i].clone());
-		}
-		
-		//column check
-		for(int i = 0; i < N; i++) {
-			int arr[] = new int[N];
-			for(int j = 0; j < N; j++) {
-				arr[j] = map[j][i];
-			}
+	private static void solution() {
+		checkRows();
+		checkCols();
+	}
+	
+	private static void checkRows() {
+		for(int r=0; r<N; r++) {
+			int c=0;
+			int nc;
 			
-			check(arr.clone());
+			boolean[] visitRow = new boolean[100];
+			
+			while(true) {
+				if(c == N-1) {
+					cnt++;
+					for(int i=0; i<N; i++) {
+						visit[r][i] = visitRow[i];
+					}
+					break;
+				}
+				
+				nc = c+1;
+				
+				if(nc >= N) break;
+				
+				int diff = map[r][c]-map[r][nc];
+				
+				if(diff == 0) { //동일한 높이
+					c=nc;
+				}
+				else if(diff == 1) { //현재 위치가 1더 높다. 앞에 경사로 놓기
+					int start = c+1;
+					int end = start+L-1;
+					
+					if(end >= N) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					boolean build = true;
+					for(int check = start; check <= end; check++) {
+						if(map[r][start] != map[r][check] || visitRow[check]) {
+							build = false;
+							break;
+						}
+					}
+					
+					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					//경사로 놓기
+					for(int check = start; check <= end; check++) {
+						visitRow[check] = true;
+					}
+					
+					c = nc; //현재 위치 다음 부터 검사
+				}
+				else if(diff == -1) { //현재 위치가 1더 낮다. 뒤에 경사로 놓기
+					int start = c-L+1;
+					int end = c;
+					
+					if(start < 0) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					boolean build = true;
+					for(int check = start; check <= end; check++) {
+						if(map[r][start] != map[r][check] || visitRow[check]) {
+							build = false;
+							break;
+						}
+					}
+					
+					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					//경사로 놓기
+					for(int check = start; check <= end; check++) {
+						visitRow[check] = true;
+					}
+					
+					c = nc; //현재 위치 다음 부터 검사
+				}
+				else { //경사로를 놓을 수 없음. 다음 행 검사
+					break;
+				}
+			}
 		}
 	}
 	
-	private static void check(int arr[]) {		
-		int preHigh = arr[0]; //직전 높이
-		int i = 1;
-		boolean buildCheck[] = new boolean[N];
-		boolean result = true;
-		
-		while(i < N) {
-			int curHigh = arr[i]; //현재 검사하는 지점 높이
+	private static void checkCols() {
+		for(int c=0; c<N; c++) {
+			int r=0;
+			int nr;
 			
-			if(preHigh == curHigh) {
-				i++;
-			}
-			else {
-				boolean canBuild = true;
+			boolean[] visitCol = new boolean[100];
+			
+			while(true) {
+				if(r == N-1) {
+					cnt++;
+					for(int i=0; i<N; i++) {
+						visit[i][c] = visitCol[i];
+					}
+					break;
+				}
 				
-				//직전 높이가 더 큰 경우
-				//현재 지점 부터 확인 후 경사로 길이 만큼 경사로를 만든다.
-				if(preHigh > curHigh) {
-					if(preHigh - curHigh > 1) {
-						result = false;
-						break;
-					}
-					
-					for(int j = i; j < i + L; j++) {
-						if(j >= N || arr[j] != curHigh) {
-							canBuild = false;
-							break;
-						}
-					}
-					
-					if(canBuild) {
-						for(int j = i; j < i + L; j++) {
-							buildCheck[j] = true; //경사로 표시
-						}
-						
-						i = i + L; //경사로 다음 부터 진행
-						preHigh = arr[i - 1]; //다음 시작 지점 이전이 경사로 이므로
-					}
-					//경사로를 놓아야할 곳에 하나라도 못 놓는다면 길은 통과할 수 없다.
-					else {
-						result = false;
-						break;
-					}
+				nr = r+1;
+				
+				if(nr >= N) break;
+				
+				int diff = map[r][c]-map[nr][c];
+				
+				if(diff == 0) { //동일한 높이
+					r=nr;
 				}
-				//현재 높이가 더 큰 경우
-				//직전 지점 부터 확인 후 경사로 길이 만큼 경사로를 만든다.
-				else {
-					if(curHigh - preHigh > 1) {
-						result = false;
-						break;
-					}
-					for(int j = i - 1; j >= i - L; j--) {
-						//직전 지점 뒤로 경사로를 만들어야 하는데 직전 지점 뒤로 범위 내에 이미 경사로가 있으면 안된다.
-						if(j < 0 || arr[j] != preHigh || buildCheck[j] == true) {
-							canBuild = false;
+				else if(diff == 1) { //현재 위치가 1더 높다. 앞에 경사로 놓기
+					int start = r+1;
+					int end = start+L-1;
+					
+					if(end >= N) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					boolean build = true;
+					for(int check = start; check <= end; check++) {
+						if(map[start][c] != map[check][c] || visitCol[check]) {
+							build = false;
 							break;
 						}
 					}
 					
-					if(canBuild) {
-						for(int j = i - 1; j >= i - L; j--) {
-							buildCheck[j] = true; //경사로 표시
+					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					//경사로 놓기
+					for(int check = start; check <= end; check++) {
+						visitCol[check] = true;
+					}
+					
+					r = nr; //현재 위치 다음 부터 검사
+				}
+				else if(diff == -1) { //현재 위치가 1더 낮다. 뒤에 경사로 놓기
+					int start = r-L+1;
+					int end = r;
+					
+					if(start < 0) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					boolean build = true;
+					for(int check = start; check <= end; check++) {
+						if(map[start][c] != map[check][c] || visitCol[check]) {
+							build = false;
+							break;
 						}
-						
-						preHigh = arr[i]; //직전에 경사로 만들었으므로
-						i++;//현재지점 다음 부터 진행						
 					}
-					//경사로를 놓아야할 곳에 하나라도 못 놓는다면 길은 통과할 수 없다.
-					else {
-						result = false;
-						break;
+					
+					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
+					
+					//경사로 놓기
+					for(int check = start; check <= end; check++) {
+						visitCol[check] = true;
 					}
+					
+					r = nr; //현재 위치 다음 부터 검사
+				}
+				else { //경사로를 놓을 수 없음. 다음 행 검사
+					break;
 				}
 			}
-		}
-		
-		if(result) {
-			cnt++;
 		}
 	}
 }
