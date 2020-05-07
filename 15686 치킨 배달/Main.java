8a72f9cdeb04dbac11aeb138296f0e659c787af7
tomo <shindohyun00@gmail.com>
2020-05-07 17:21:29 +0900

    15686 치킨 배달 재풀이

--- 15686 치킨 배달/Main.java
+++ 15686 치킨 배달/Main.java
@@ -1,100 +1,90 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int map[][];
-	static int cCnt = 0; //치킨집 개수
-	static int min = 99999;
+	static class Point{
+		public int r, c;
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		map = new int[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				int input = sc.nextInt();
-				
+	static int N; //2~50
+	static int M; //1~13
+	static int[][] map = new int[50][50];
+	static Point[] chickens = new Point[13];
+	static int chickenCnt = 0;
+	static int min = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				int input = Integer.parseInt(st.nextToken());
 				if(input == 2) {
-					cCnt++;
+					chickens[chickenCnt++] = new Point(i,j);
 				}
-				
-				map[i][j] = input;
+				else {
+					map[i][j] = input;
+				}
 			}
 		}
-		sc.close();
+		br.close();
 		
 		solution();
 		System.out.println(min);
 	}
 	
 	private static void solution() {
-		dfs(0, 0, 0);
+		combination(0, 0);
 	}
 	
-	//한개씩 폐업시키면서 최소 도시 치킨 거리를 찾는다.
-	private static void dfs(int delCnt, int x, int y) {
-		//cCnt - M = 폐업하는 치킨 집 개수
-		if(delCnt == cCnt - M) {
-			//최소 도시 치킨 거리 구하기
-			int sum = 0;
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					//집일 때
-					if(map[i][j] == 1) {
-						//각 치킨집까지의 거리를 비교하여 최소를 찾는다.
-						int tmp_min = 99999;
-						for(int a = 0; a < N; a++) {
-							for(int b = 0; b < N; b++) {
-								if(map[a][b] == 2) {
-									int hX = j;
-									int hY = i;
-									int cX = b;
-									int cY = a;
-									
-									int d = Math.abs(hX - cX) + Math.abs(hY - cY);
-									if(tmp_min > d) {
-										tmp_min = d;
-									}
-								}
-							}
-						}
-						//최소를 더한다.
-						sum += tmp_min;
+	private static void combination(int cnt, int idx) {
+		if(cnt == M) {
+			int total = 0;
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					if(map[i][j] != 1) continue;
+					
+					int minDis = Integer.MAX_VALUE;
+					
+					for(int z=0; z<chickenCnt; z++) {
+						if(map[chickens[z].r][chickens[z].c]!=2) continue;
+						int dis = Math.abs(i-chickens[z].r) + Math.abs(j-chickens[z].c);
+						if(dis < minDis) minDis=dis;
 					}
+					
+					total += minDis;
 				}
 			}
 			
-			if(min > sum) {
-				min = sum;
-			}
+			if(min > total) min = total;
 			
 			return;
 		}
 		
-		//주의) 매개변수로 넘어온 y, x 로 시작하지 않으면 시간 초과!!
-		for(int i = y; i < N; i++) {
-			for(int j = x; j < N; j++) {
-				//치킨집인 경우 
-				if(map[i][j] == 2) {
-					//폐업시켜본다.
-					map[i][j] = 0;
-					
-					dfs(delCnt + 1, j, i);
-					
-					//복구시킨다.
-					map[i][j] = 2;
-				}
-			}
-			x = 0; //주의) 항상 주의!!
-		}
+		if(cnt > M || idx >= chickenCnt || map[chickens[idx].r][chickens[idx].c]==2) return;
+		
+		map[chickens[idx].r][chickens[idx].c] = 2;
+		combination(cnt+1, idx+1);
+		map[chickens[idx].r][chickens[idx].c] = 0;
+		
+		combination(cnt, idx+1);
 	}
 	
 	private static void printMap() {
 		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				System.out.print(map[i][j] + " ");
+			for(int j=0; j< N; j++) {
+				System.out.print(map[i][j]+" ");
 			}
 			System.out.println();
 		}
