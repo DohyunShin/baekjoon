c06767f2c57dbec596f7ccbbb347e98bb0ea3a23
tomo <shindohyun00@gmail.com>
2020-05-16 03:24:02 +0900

    14502 연구소 재풀이

--- 14502 연구소/Main.java
+++ 14502 연구소/Main.java
@@ -6,24 +6,34 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static class Point{
+		public int r,c;
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
 	static int N, M; //3~8
 	static int[][] map = new int[8][8];
 	static int max = Integer.MIN_VALUE;
+	static int[] dr= {-1,1,0,0};
+	static int[] dc= {0,0,-1,1};
 	static int emptyCnt = 0;
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		for(int i=0;i<N;i++) {
+		int input;
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<M;j++) {
-				int input = Integer.parseInt(st.nextToken());
-				if(input == 0) emptyCnt++;
+			for(int j=0; j<M; j++) {
+				input = Integer.parseInt(st.nextToken());
 				map[i][j] = input;
+				if(input==0)emptyCnt++;
 			}
 		}
 		br.close();
@@ -33,69 +43,78 @@ public class Main {
 	}
 	
 	private static void solution() {
-		recursive(0,0,0,emptyCnt);
+		recursive(0,0,0);
 	}
 	
-	private static void recursive(int sr, int sc, int wallCnt, int curEmptyCnt) {
-		if(wallCnt == 3) {
-			//바이러스 확장
-			int[][] curMap = map.clone();
-			for(int i = 0; i < N; i++) {
-				curMap[i] = map[i].clone();
-			}
-			Queue<Point> q = new LinkedList<Point>();
-			for(int r=0;r<N;r++) {
-				for(int c=0;c<M;c++) {
-					if(curMap[r][c] != 2) continue;
-					q.add(new Point(r,c));
-				}
-			}
-			bfs(curMap, q, curEmptyCnt);			
+	private static void recursive(int cnt, int cr, int cc) {
+		if(cnt == 3) {
+			bfs();
 			return;
 		}
 		
-		if(!(sr>=0&&sr<N&&sc>=0&&sc<M)) return;
+		if(cnt > 3) return;
 		
-		int nr = sr;
-		int nc = sc+1;
-		if(nc >= M) {
-			nc = 0;
-			nr++;
+		
+		for(int r=cr; r<N; r++) {
+			for(int c=cc; c<M; c++) {
+				if(map[r][c] != 0) continue;
+				
+				map[r][c] = 1;
+				recursive(cnt+1, r, c+1);
+				map[r][c] = 0;
+			}
+			cc=0;
 		}
 		
-		if(map[sr][sc] == 0) {
-			//벽을 세우는 경우
-			map[sr][sc] = 1;
-			recursive(nr,nc,wallCnt+1, curEmptyCnt-1);
-			map[sr][sc] = 0;
+	}
+	
+	private static void bfs() {
+		int curEmptyCnt = emptyCnt-3; // 새로운 3개의 기둥 개수 빼주기
+		
+		int[][] copyMap = map.clone();
+		for(int i = 0; i < N; i++) {
+			copyMap[i] = map[i].clone();
+		}
+				
+		Queue<Point> q = new LinkedList<Point>();
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(copyMap[i][j] == 2) {
+					q.add(new Point(i, j));
+				}
+			}
 		}
 		
-		//벽을 세우지 않는 경우
-		recursive(nr,nc,wallCnt, curEmptyCnt);
-	}
-	
-	static class Point{
-		int r,c;
-		public Point(int r, int c) {this.r=r; this.c=c;}
-	}
-	
-	private static void bfs(int[][] curMap, Queue<Point> q, int curEmptyCnt) {
+		Point curPt;
+		
 		while(!q.isEmpty()) {
-			Point v = q.poll();
+			curPt = q.poll();
 			
-			int nr, nc;
-			for(int d=0;d<4;d++) {
-				nr = v.r+dr[d];
-				nc = v.c+dc[d];
+			int nr,nc;
+			for(int d=0; d<4; d++) {
+				nr = curPt.r+dr[d];
+				nc = curPt.c+dc[d];
 				
-				if(!(nr>=0&&nr<N&&nc>=0&&nc<M) || curMap[nr][nc] != 0) continue;
+				if(!(nr>=0&&nr<N&&nc>=0&&nc<M)||copyMap[nr][nc] != 0) continue;
 				
-				curMap[nr][nc] = 2;
-				curEmptyCnt--;
+				copyMap[nr][nc] = 2;
 				q.add(new Point(nr,nc));
+				
+				curEmptyCnt--; //바이러스가 확장된 빈 공간 개수 빼기
 			}
 		}
 		
 		if(curEmptyCnt > max) max = curEmptyCnt;
+	}
+	
+	private static void printMap(int[][] curMap) {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				System.out.print(curMap[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
