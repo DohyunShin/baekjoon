a52dd26cf8e424e4e5b44ecb002ead5131755220
tomo <shindohyun00@gmail.com>
2020-05-17 20:34:21 +0900

    6603 로또 재풀이

--- 6603 로또/Main.java
+++ 6603 로또/Main.java
@@ -4,41 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] s = new int[49];
-	static boolean[] visit = new boolean[49];
-	static int k = 0;
+	static int k; //7~12
+	static int[] S = new int[12];
+	static boolean[] visit = new boolean[12];
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-
-		while(true) {
+		do {
 			st = new StringTokenizer(br.readLine());
 			k = Integer.parseInt(st.nextToken());
 			if(k == 0) break;
-			
-			for(int i = 0; i < k; i++) {
-				s[i] = Integer.parseInt(st.nextToken());
+			for(int i=0; i<k; i++) {
+				S[i] = Integer.parseInt(st.nextToken());
 			}
-			
 			solution();
-		}
-		
+			sb.append('\n');
+		}while(true);
 		br.close();
+		
+		System.out.println(sb);
 	}
-	
-	static void solution() {
+
+	private static void solution() {
 		combination(0, 0);
-		System.out.println();
 	}
 	
-	static void combination(int cnt, int idx) {
+	private static void combination(int cnt, int idx) {
 		if(cnt == 6) {
-			for(int i = 0; i < k; i++) {
-				if(!visit[i]) continue;
-				System.out.print(s[i] + " ");
+			for(int i=0; i<k; i++) {
+				if(visit[i]) {
+					sb.append(S[i] + " ");
+				}
 			}
-			System.out.println();
+			sb.append('\n');
 			return;
 		}
 		
@@ -50,5 +50,4 @@ public class Main {
 		
 		combination(cnt, idx+1);
 	}
-	
 }
