c980eb703bb6a677ed5bfabf25205342588e0d2c
tomo <shindohyun00@gmail.com>
2020-07-24 02:09:00 +0900

    17825 주사위 윷놀이 재풀이

--- 17825 주사위 윷놀이/Main.java
+++ 17825 주사위 윷놀이/Main.java
@@ -1,120 +1,138 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int cases[] = new int[10];
-	static int max = 0;
-	static final int FINAL = 100;
-	static final int FINAL_POS = 21;
 	static final int NODE_CNT = 33;
+	static final int RED = 0;
 	static final int BLUE = 1;
-	static final int NEXT = 0;
-	static int map[][] = new int[NODE_CNT][2];
-	static int scoreMap[] = new int[NODE_CNT];
-	static boolean blueStart[] = new boolean[NODE_CNT];
-	static int unitPos[] = new int[4];
+	static final int END_IDX = 21;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int[][] path = new int[NODE_CNT][2];
+	static int[] score = new int[NODE_CNT];
+	static int[] nums = new int[10];
+	static int res = 0;
+	static Set<Integer> blues = new HashSet<Integer>();
+	static int[] units = new int[4]; //4개의 말 위치 저장 (노드의 인덱스 저장)
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < 10; i++) {
-			cases[i] = sc.nextInt();
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		//경로 세팅(화살표 방향으로)
-		for(int i = 0; i < NODE_CNT; i++) {
-			map[i][NEXT] = i + 1;
+		//RED 경로 세팅, 점수 저장
+		for(int i = 0; i < END_IDX; i++) {
+			path[i][RED] = i+1; //시계방향으로
+			score[i] = 2*i;
 		}
-		map[28][NEXT] = 27;
-		map[27][NEXT] = 26;
-		map[26][NEXT] = 25;
-		map[30][NEXT] = 25;
-		map[25][NEXT] = 31;
-		map[32][NEXT] = 20;
-		//파란색 경로로 나눠지는 구간 세팅
-		map[5][BLUE] = 22;
-		map[10][BLUE] = 29;
-		map[15][BLUE] = 28;
+		score[END_IDX] = 0;
 		
-		//도착 지점
-		map[FINAL_POS][0] = FINAL_POS;
-				
-		//파란색 지점 표시
-		blueStart[5] = true;
-		blueStart[10] = true;
-		blueStart[15] = true;
+		path[END_IDX][RED] = END_IDX; //도착한 말은 제자리
+		//십자가 왼->오, 아래->위, 점수 저
+		path[22][RED] = 23;
+		path[23][RED] = 24;
+		path[24][RED] = 25;
+		path[28][RED] = 27;
+		path[27][RED] = 26;
+		path[26][RED] = 25;
+		path[29][RED] = 30;
+		path[30][RED] = 25;
+		path[25][RED] = 31;
+		path[31][RED] = 32;
+		path[32][RED] = 20;
+		score[22] = 13;
+		score[23] = 16;
+		score[24] = 19;
+		score[25] = 25;
+		score[26] = 26;
+		score[27] = 27;
+		score[28] = 28;
+		score[29] = 22;
+		score[30] = 24;
+		score[31] = 30;
+		score[32] = 35;
+		//BLUE 경로 세팅
+		path[5][BLUE] = 22;
+		path[10][BLUE] = 29;
+		path[15][BLUE] = 28;
 		
-		//점수 세팅
-		for(int i = 1; i <= 20; i++) {
-			scoreMap[i] = i * 2;
-		}
-		scoreMap[22] = 13;
-		scoreMap[23] = 16;
-		scoreMap[24] = 19;
-		scoreMap[25] = 25;
-		scoreMap[26] = 26;
-		scoreMap[27] = 27;
-		scoreMap[28] = 28;
-		scoreMap[29] = 22;
-		scoreMap[30] = 24;
-		scoreMap[31] = 30;
-		scoreMap[32] = 35;
+		//BLUE 인덱스 저장
+		blues.add(5);
+		blues.add(10);
+		blues.add(15);
 		
-		sc.close();
+		
+		br.close();
 		
 		solution();
-		System.out.println(max);
+		System.out.println(res);
 	}
 	
-	private static void solution() {
-		dfs(0, 0);
+	public static void solution() {
+		permutation(0, 0);
 	}
 	
-	private static void dfs(int cnt, int score) {
+	public static void permutation(int cnt, int sum) {
 		if(cnt == 10) {
-			if(max < score) {
-				max = score;
-			}
+			if(res < sum) res = sum;
 			return;
 		}
 		
-		
-		
 		for(int i = 0; i < 4; i++) {
-			int moveCnt = cases[cnt]; //이동해야할 횟수
-			final int curPos = unitPos[i];
-			int targetPos = curPos; //이동할 위치
+			int curIdx = units[i];
 			
-			//시작 위치가 파란색인지 확인
-			if(blueStart[curPos] == true) {
-				//시작 위치가 파란색인 경우 파란색 경로의 시작 지점으로 이동할 위치를 옮긴다.
-				targetPos = map[curPos][BLUE];
-				moveCnt--;
-			}
+			//도착한 말은 선택하지 않는다.
+			if(units[i] == END_IDX) continue;
 			
-			//최종적으로 이동할 위치를 찾는다.
-			while(moveCnt-- > 0) {
-				targetPos = map[targetPos][NEXT];
-			}
+			int nextIdx = move(nums[cnt], curIdx, blues.contains(curIdx));
 			
-			//이동하려는 위치에 말이 있는 경우 해당 말을 이동하지 않는다.
-			boolean already = false;
-			for(int j = 0; j < 4; j++) {
-				if(unitPos[j] == targetPos && targetPos != FINAL_POS) {
-					already = true;
-					break;
-				}
-			}
-			if(already) {
+			if(isOccupied(nextIdx)) {
+				//선택 불가
 				continue;
 			}
-			
-			//해당 말을 이동하려는 위치로 변경한다.
-			unitPos[i] = targetPos;
-			
-			//이동한 위치의 점수를 더해주고 다음을 케이스를 진행한다.
-			dfs(cnt + 1, score + scoreMap[targetPos]);
-			
-			//해당 말을 원래 위치로 돌려놓는다.
-			unitPos[i] = curPos;
+			else {
+				//이동하고 점수 누적
+				units[i] = nextIdx;			
+				
+				permutation(cnt+1, sum+score[nextIdx]);
+				
+				//다른 말을 선택하는 경우를 위해 이동한 말을 원복한다.
+				units[i] = curIdx;
+			}
 		}
+	}
+	
+	//다른 말이 점령하고 있는 노드인지 확인한다.
+	//노드가 도착 노드이면 예외
+	public static boolean isOccupied(int idx) {
+		if(idx == END_IDX) return false;
+		
+		for(int i = 0; i < 4; i++) {
+			if(units[i] == idx) return true;
+		}
+		return false;
+	}
+	
+	//주사위 숫자 만큼 이동한다.
+	//출발 노드가 파란색인 경우 파란색 경로로 이동한다.
+	public static int move(int num, int curIdx, boolean isBlue) {		
+		int nextIdx = 0;
+		
+		if(isBlue) {
+			nextIdx = path[curIdx][BLUE];
+			num--;
+			curIdx = nextIdx;
+		}
+		
+		for(int i = 0; i < num; i++) {
+			nextIdx = path[curIdx][RED];
+			curIdx = nextIdx;
+		}
+		
+		return nextIdx;
 	}
 }
