770f144869b7479f0477feef67e6a14e440337bc
tomo <shindohyun00@gmail.com>
2020-09-19 18:41:30 +0900

    14890 경사로 재풀이

--- 14890 경사로/Main.java
+++ 14890 경사로/Main.java
@@ -4,188 +4,120 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N; //2~100
-	static int L; //1~100
-	static int[][] map = new int[100][100]; //각 1~10
-	static boolean[][] visit = new boolean[100][100]; //사다리 체크
-	static int cnt =0;
+	static int N; // 2~100
+	static int L; // 1~100
+	static int[][] map = new int[100][100];
+	static boolean[][] visit = new boolean[100][100];
+	static int res = 0;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st =new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
-		for(int i =0;i<N;i++) {
-			st =new StringTokenizer(br.readLine());
-			for(int j=0;j<N;j++) {
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		br.close();
 		
 		solution();
-		System.out.println(cnt);
+		System.out.println(res);
 	}
 	
 	private static void solution() {
-		checkRows();
-		checkCols();
-	}
-	
-	private static void checkRows() {
-		for(int r=0; r<N; r++) {
-			int c=0;
-			int nc;
-			
-			boolean[] visitRow = new boolean[100];
-			
-			while(true) {
-				if(c == N-1) {
-					cnt++;
-					for(int i=0; i<N; i++) {
-						visit[r][i] = visitRow[i];
-					}
-					break;
-				}
-				
-				nc = c+1;
-				
-				if(nc >= N) break;
-				
-				int diff = map[r][c]-map[r][nc];
-				
-				if(diff == 0) { //동일한 높이
-					c=nc;
-				}
-				else if(diff == 1) { //현재 위치가 1더 높다. 앞에 경사로 놓기
-					int start = c+1;
-					int end = start+L-1;
-					
-					if(end >= N) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					boolean build = true;
-					for(int check = start; check <= end; check++) {
-						if(map[r][start] != map[r][check] || visitRow[check]) {
-							build = false;
-							break;
-						}
-					}
-					
-					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					//경사로 놓기
-					for(int check = start; check <= end; check++) {
-						visitRow[check] = true;
-					}
-					
-					c = nc; //현재 위치 다음 부터 검사
-				}
-				else if(diff == -1) { //현재 위치가 1더 낮다. 뒤에 경사로 놓기
-					int start = c-L+1;
-					int end = c;
-					
-					if(start < 0) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					boolean build = true;
-					for(int check = start; check <= end; check++) {
-						if(map[r][start] != map[r][check] || visitRow[check]) {
-							build = false;
-							break;
-						}
-					}
-					
-					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					//경사로 놓기
-					for(int check = start; check <= end; check++) {
-						visitRow[check] = true;
-					}
-					
-					c = nc; //현재 위치 다음 부터 검사
-				}
-				else { //경사로를 놓을 수 없음. 다음 행 검사
-					break;
-				}
-			}
+		for(int i = 0; i < N; i++) {
+			if(check(i)) res++;
+			//printVisit();
+		}
+		
+		rotate(); // 90도 회전
+		//printMap();
+		for(int i = 0; i < N; i++) {
+			if(check(i)) res++;
+			//printVisit();
 		}
 	}
 	
-	private static void checkCols() {
-		for(int c=0; c<N; c++) {
-			int r=0;
-			int nr;
+	private static boolean check(int r) {
+		int[] path = map[r];
+		boolean[] temp = new boolean[N];
+		
+		int high = 0;
+		
+		for(int i = 0; i < N; i++) {
+			if(high == 0) {
+				high = path[i];
+				continue;
+			}
 			
-			boolean[] visitCol = new boolean[100];
+			int diff = high - path[i];
 			
-			while(true) {
-				if(r == N-1) {
-					cnt++;
-					for(int i=0; i<N; i++) {
-						visit[i][c] = visitCol[i];
-					}
-					break;
+			if(diff == 0) continue;
+			else if(Math.abs(diff) != 1) return false;
+			// 현재 위치로 올라오는 경사로를 현재 위치의 이전 부터 뒤로 놓는다.
+			else if(diff < 0) {
+				boolean can = true;
+				for(int j = i-1; j > i-1-L; j--) {
+					if(!(j >= 0)) return false;
+					if(high != path[j]) return false;
+					if(temp[j]) return false;
+					temp[j] = true;	
 				}
-				
-				nr = r+1;
-				
-				if(nr >= N) break;
-				
-				int diff = map[r][c]-map[nr][c];
-				
-				if(diff == 0) { //동일한 높이
-					r=nr;
+				high = path[i];
+			}
+			// 다음 위치로 내려가는 경사로를 현재 위치 부터 앞으로 놓는다.
+			else if(diff > 0) {
+				int j = i;
+				for(; j < i+L; j++) {
+					if(!(j < N)) return false;
+					if(path[i] != path[j]) return false;
+					if(temp[j]) return false;
+					temp[j] = true;
 				}
-				else if(diff == 1) { //현재 위치가 1더 높다. 앞에 경사로 놓기
-					int start = r+1;
-					int end = start+L-1;
-					
-					if(end >= N) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					boolean build = true;
-					for(int check = start; check <= end; check++) {
-						if(map[start][c] != map[check][c] || visitCol[check]) {
-							build = false;
-							break;
-						}
-					}
-					
-					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					//경사로 놓기
-					for(int check = start; check <= end; check++) {
-						visitCol[check] = true;
-					}
-					
-					r = nr; //현재 위치 다음 부터 검사
-				}
-				else if(diff == -1) { //현재 위치가 1더 낮다. 뒤에 경사로 놓기
-					int start = r-L+1;
-					int end = r;
-					
-					if(start < 0) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					boolean build = true;
-					for(int check = start; check <= end; check++) {
-						if(map[start][c] != map[check][c] || visitCol[check]) {
-							build = false;
-							break;
-						}
-					}
-					
-					if(!build) break; //경사로를 놓을 수 없음. 다음 행 검사
-					
-					//경사로 놓기
-					for(int check = start; check <= end; check++) {
-						visitCol[check] = true;
-					}
-					
-					r = nr; //현재 위치 다음 부터 검사
-				}
-				else { //경사로를 놓을 수 없음. 다음 행 검사
-					break;
-				}
+				high = path[i];
 			}
 		}
+		
+		visit[r] = temp.clone();
+		return true;
+	}
+	
+	private static void rotate() {
+		int[][] newMap = new int[N][N];
+		boolean[][] newVisit = new boolean[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				newMap[j][N-1-i] = map[i][j];
+				newVisit[j][N-1-i] = visit[i][j];
+			}
+		}
+		
+		map = newMap;
+		visit = newVisit;
+	}
+	
+	private static void printVisit() {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				System.out.print(visit[i][j]?"1 " : "0 ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	private static void printMap() {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
